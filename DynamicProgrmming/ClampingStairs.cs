e72ed98ae0b7fb0fd267e3be0db927d7255dbd3a
Amr Elmenbawy <amr.elmenbawy@excp.sa>
2025-11-20 00:06:03 +0200

    Add ClampingStairs class with CountWays method
    Introduced a new `ClampingStairs` class in the `ProblemSolvingSolution.DynamicProgrmming` namespace.
    The class includes a static method `CountWays` that takes an integer parameter `n` and currently returns `0`.
    Added `using` directives for `System`, `System.Collections.Generic`, and `System.Text`.

---
+++ DynamicProgrmming/ClampingStairs.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProblemSolvingSolution.DynamicProgrmming
+{
+    public class ClampingStairs
+    {
+
+        public static int CountWays(int n)
+        {
+            return 0;
+         }
+    }
+}
